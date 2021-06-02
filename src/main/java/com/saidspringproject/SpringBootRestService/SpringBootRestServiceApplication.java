bbd758846d286e04f7018ece1cb28c53cf6e5dcf
Said Adem <saidadem3@gmail.com>
2021-06-02 07:33:25 -0500

    Introduced to JPA

--- src/main/java/com/saidspringproject/SpringBootRestService/SpringBootRestServiceApplication.java
+++ src/main/java/com/saidspringproject/SpringBootRestService/SpringBootRestServiceApplication.java
@@ -1,13 +1,50 @@
 package com.saidspringproject.SpringBootRestService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootRestServiceApplication {
+import com.saidspringproject.SpringBootRestService.controller.Library;
+import com.saidspringproject.SpringBootRestService.repository.LibraryRepository;
 
+@SpringBootApplication
+public class SpringBootRestServiceApplication implements CommandLineRunner{
+
+	@Autowired
+	LibraryRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestServiceApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String[] args)
+	{
+		Library lib = repository.findById("fdsefr343").get();
+		System.out.println("Java freaking sucks"
+				+"\nBut here we are, just to suffer"
+				+"\nBtw here's the author = "+ lib.getAuthor());
+		
+		Library en = new Library();
+		en.setAisle(123);
+		en.setAuthor("Nimesh");
+		en.setBook_name("Devops");
+		en.setIsbn("lkhs");
+		en.setId("lkhs123");
+		//repository.save(en);
+		
+		List<Library> allrecords = repository.findAll();
+		
+		//repository.delete(lib);
+		for(Library item: allrecords)
+		{
+			System.out.println(item.getBook_name());
+		}
+		
+	}
 }
+
+
---
+++ src/main/java/com/saidspringproject/SpringBootRestService/repository/LibraryRepository.java
@@ -0,0 +1,9 @@
+package com.saidspringproject.SpringBootRestService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saidspringproject.SpringBootRestService.controller.Library;
+
+public interface LibraryRepository extends JpaRepository<Library, String> {
+
+}
